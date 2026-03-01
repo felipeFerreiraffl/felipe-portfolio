0c2cf59062e7edab6e99eaa6693da7ff67526d4e
felipeFerreiraffl <felipado2x0@gmail.com>
2026-03-01 00:33:52 -0300

    chore: more styles for shadow, new translations and deletion of global nav links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,13 +1,7 @@
+import { useTranslations } from "next-intl";
+
 export const LINKS = {
   github: "https://github.com/felipeFerreiraffl",
   linkedin: "https://www.linkedin.com/in/felipe-ferreira-959bb8271/",
   mail: "felipe.ferr.lima04@gmail.com",
 };
-
-export const NAV_LINKS = [
-  { label: "Sobre mim", link: "#" },
-  { label: "Experiências", link: "#" },
-  { label: "Habilidades", link: "#" },
-  { label: "Projetos", link: "#" },
-  { label: "Contato", link: "#" },
-];
