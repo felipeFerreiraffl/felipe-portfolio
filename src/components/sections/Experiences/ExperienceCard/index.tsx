a70748c80787787651beb787e8763946ab51b45b
felipeFerreiraffl <felipado2x0@gmail.com>
2026-03-05 22:22:55 -0300

    feat(experiences): new component for experiences card

---
+++ src/components/sections/Experiences/ExperienceCard/index.tsx
@@ -0,0 +1,75 @@
+import { imageUrl } from "@/utils/imageUrl";
+import { StaticImageData } from "next/image";
+import { CSSProperties } from "react";
+
+type ExperienceCardProps = {
+  index: number;
+  title: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  illustration: StaticImageData | string;
+};
+
+export default function ExperienceCard({
+  index,
+  title,
+  description,
+  startDate,
+  endDate,
+  illustration,
+}: ExperienceCardProps) {
+  const paddingNum = index.toString().padStart(2, "0");
+
+  return (
+    <div className="relative lg:max-w-[80dvw] lg:w-310 w-full md:h-155 h-145 flex items-start justify-between border-[1.5px] border-line rounded-sm bg-bg overflow-hidden">
+      <span className="absolute -top-2 -left-2 md:size-16 size-12 flex items-center justify-center bg-bg border-[1.5px] border-line rounded-sm font-heading font-bold leading-heading text-white text-heading-h3 text-center">
+        {paddingNum}
+      </span>
+
+      <div className="lg:max-w-1/2 md:max-w-[80%] max-w-full flex flex-col md:items-start items-center justify-between gap-10 md:px-10 md:py-20 px-5 py-10">
+        <div className="flex flex-col md:items-start items-center gap-3">
+          <h3 className="font-heading font-bold leading-heading md:text-heading-h2 text-heading-h3 md:text-left text-center text-white">
+            {title}
+          </h3>
+          <span className="font-body font-normal leading-body text-sm text-white">
+            {startDate} — {endDate}
+          </span>
+        </div>
+
+        <div className="flex flex-col md:items-start items-center gap-8">
+          <div className="flex flex-col md:items-start items-center gap-2">
+            <h4 className="font-heading font-medium leading-heading md:text-2xl text-xl text-white">
+              Descrição
+            </h4>
+            <p className="font-body font-normal leading-body md:text-base text-sm text-white">
+              {description}
+            </p>
+          </div>
+
+          <div className="flex flex-col md:items-start items-center gap-2">
+            <h4 className="font-heading font-medium leading-heading md:text-2xl text-xl text-white">
+              Habilidades
+            </h4>
+            <div className="flex flex-wrap md:items-start items-center gap-5">
+              <p className="bg-bg font-body font-normal md:text-base text-sm text-white border border-main rounded-sm shadow-normal px-3 py-2">
+                Teste
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="relative aspect-portrait h-full opacity-40 clip-path-illustration lg:block hidden"
+        style={{}}>
+        <div className="absolute top-0 right-0 size-full bg-main/40"></div>
+        <div
+          className="size-full bg-(image:--bg-img) bg-center bg-cover bg-no-repeat opacity-60"
+          style={
+            { "--bg-img": `url(${imageUrl(illustration)})` } as CSSProperties
+          }></div>
+      </div>
+    </div>
+  );
+}
