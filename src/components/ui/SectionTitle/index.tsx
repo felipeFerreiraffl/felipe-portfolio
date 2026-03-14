ec1ae2a49cd9ae8f79f05ac6c4cdf5afed3b2ffd
felipeFerreiraffl <felipado2x0@gmail.com>
2026-03-14 00:35:15 -0300

    style: line animation on section title component

--- src/components/ui/SectionTitle/index.tsx
+++ src/components/ui/SectionTitle/index.tsx
@@ -1,6 +1,8 @@
 import { IconSVGElement } from "@/types/elements.types";
 import { useTranslations } from "next-intl";
 import Icon from "../Icon";
+import * as m from "motion/react-m";
+import { motionVariants } from "@/utils/motionVariants";
 
 type SectionTitleProps = {
   title: string;
@@ -15,6 +17,12 @@ export default function SectionTitle({
 }: SectionTitleProps) {
   const t = useTranslations("Common");
 
+  const lineVariants = motionVariants({
+    initial: { scaleX: 0 },
+    animation: { scaleX: 1, transition: { ease: "easeInOut", duration: 0.8 } },
+    exit: { scaleX: 0 },
+  });
+
   return (
     <div className="w-full flex mx-auto md:items-start items-center md:flex-row flex-col md:gap-0 gap-3">
       <Icon
@@ -27,7 +35,14 @@ export default function SectionTitle({
         <span className="font-body font-normal leading-body md:text-sm text-white md:pl-5">
           {t("sectionChapter", { number: chapterNumber })}
         </span>
-        <hr className="border-none h-[1.5px] bg-main w-full" />
+        <m.hr
+          variants={lineVariants}
+          initial="start"
+          whileInView="middle"
+          exit="end"
+          viewport={{ amount: 0.8 }}
+          className="border-none h-[1.5px] bg-main w-full origin-left"
+        />
         <h2 className="font-heading font-bold leading-heading lg:text-5xl md:text-heading-h2 text-heading-h3 text-white uppercase md:pl-5">
           {title}
         </h2>
